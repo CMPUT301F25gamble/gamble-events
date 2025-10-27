41d0f67c7d32bf6ad4f5bbebd52103ab48263346
GaurangBhana <bhana@ualberta.ca>
2025-10-26 20:19:33 -0600

    Added more functionality to all of the code and added EntrantList class

--- EventLotterySystemApplication/app/src/main/java/com/example/eventlotterysystemapplication/Event.java
+++ EventLotterySystemApplication/app/src/main/java/com/example/eventlotterysystemapplication/Event.java
@@ -1,4 +1,25 @@
 package com.example.eventlotterysystemapplication;
 
+import java.time.LocalDateTime;
+
 public class Event {
+    private String name;
+    private String description;
+    private LocalDateTime eventTime;
+    private LocalDateTime signupDeadline;
+    private LocalDateTime invitationAcceptanceDeadline;
+    private User organizer;
+    private String place;
+    private EntrantList entrantList;
+    private int maxWaitingListCapacity;
+    private int maxFinalListCapacity;
+
+    /*
+    Include code to have some attributes that points to an event poster, I wouldn't know how to
+    declare attributes of that type yet
+     */
+
+    /*
+    Geolocation requirement
+     */
 }

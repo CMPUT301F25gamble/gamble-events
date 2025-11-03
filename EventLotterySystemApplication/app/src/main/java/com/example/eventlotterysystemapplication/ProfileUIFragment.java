e3334841cb1708d8bbcb1160ec5628f4111c6809
Ostrich <theamazingostrich1231@gmail.com>
2025-11-03 10:42:44 -0700

    Added content descriptions for image buttons in fragment_profile_ui.xml

--- EventLotterySystemApplication/app/src/main/java/com/example/eventlotterysystemapplication/ProfileUIFragment.java
+++ EventLotterySystemApplication/app/src/main/java/com/example/eventlotterysystemapplication/ProfileUIFragment.java
@@ -8,6 +8,9 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.example.eventlotterysystemapplication.databinding.FragmentProfileUiBinding;
 
@@ -19,6 +22,7 @@ import com.example.eventlotterysystemapplication.databinding.FragmentProfileUiBi
 public class ProfileUIFragment extends Fragment {
 
     private FragmentProfileUiBinding binding;
+    private Database db;
 
     public ProfileUIFragment() {
         // Required empty public constructor
@@ -33,6 +37,7 @@ public class ProfileUIFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        db = Database.getDatabase();
     }
 
     @Override
@@ -41,5 +46,22 @@ public class ProfileUIFragment extends Fragment {
         // Inflate the layout for this fragment
         binding = FragmentProfileUiBinding.inflate(inflater, container, false);
         return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        // Make buttons
+        ImageButton nameEditButton = binding.nameEditButton;
+        ImageButton emailEditButton = binding.emailEditButton;
+        ImageButton phoneEditButton = binding.phoneEditButton;
+        Button showHistoryButton = binding.showProfileHistoryButton;
+        Button deleteAccountButton = binding.deleteProfileButton;
+
+        // Replace placeholder text
+        TextView userName = binding.profileName;
+        TextView userEmail = binding.profileEmail;
+        TextView userPhone = binding.profilePhone;
+
+        // Todo: Retrieve user info from database and replace placeholder text
     }
 }

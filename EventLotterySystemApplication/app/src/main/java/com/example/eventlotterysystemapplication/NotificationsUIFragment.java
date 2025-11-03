6a7f8925c748a94d280ca4de0f5782aa0ed28dd9
Ostrich <theamazingostrich1231@gmail.com>
2025-11-02 19:38:59 -0700

    Fixed typo in main_nav_graph.xml, changed nav graph name to nav_graph.xml

--- EventLotterySystemApplication/app/src/main/java/com/example/eventlotterysystemapplication/NotificationsUIFragment.java
+++ EventLotterySystemApplication/app/src/main/java/com/example/eventlotterysystemapplication/NotificationsUIFragment.java
@@ -59,6 +59,6 @@ public class NotificationsUIFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_notifcations_ui, container, false);
+        return inflater.inflate(R.layout.fragment_notifications_ui, container, false);
     }
 }

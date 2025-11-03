ff00d8ddf6fb7bf13111d62e9838a3940326f567
Ostrich <theamazingostrich1231@gmail.com>
2025-11-03 10:23:31 -0700

    Removed starter code in ProfileUIFragment.java

--- EventLotterySystemApplication/app/src/main/java/com/example/eventlotterysystemapplication/ProfileUIFragment.java
+++ EventLotterySystemApplication/app/src/main/java/com/example/eventlotterysystemapplication/ProfileUIFragment.java
@@ -2,11 +2,14 @@ package com.example.eventlotterysystemapplication;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.eventlotterysystemapplication.databinding.FragmentProfileUiBinding;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,50 +18,28 @@ import android.view.ViewGroup;
  */
 public class ProfileUIFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private FragmentProfileUiBinding binding;
 
     public ProfileUIFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ProfileUI.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ProfileUIFragment newInstance(String param1, String param2) {
+    public static ProfileUIFragment newInstance() {
         ProfileUIFragment fragment = new ProfileUIFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile_ui, container, false);
+        binding = FragmentProfileUiBinding.inflate(inflater, container, false);
+        return binding.getRoot();
     }
 }

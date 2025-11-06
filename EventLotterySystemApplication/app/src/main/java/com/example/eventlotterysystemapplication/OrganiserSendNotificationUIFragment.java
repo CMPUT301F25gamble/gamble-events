ea3c62f4495470d1e2ba971c93251646d933426f
Ostrich <theamazingostrich1231@gmail.com>
2025-11-06 16:16:16 -0700

    Created organiser send notification

--- EventLotterySystemApplication/app/src/main/java/com/example/eventlotterysystemapplication/OrganiserSendNotificationUIFragment.java
+++ EventLotterySystemApplication/app/src/main/java/com/example/eventlotterysystemapplication/OrganiserSendNotificationUIFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -50,8 +52,10 @@ public class OrganiserSendNotificationUIFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        // Get header text
+        // Get views
         TextView notificationHeader = binding.sendNotificationHeader;
+        EditText notificationMessage = binding.notificationMessageContent;
+        Button sendNotificationButton = binding.sendNotificationButton;  // get the button
 
         // Set header text
         switch (notificationType) {
@@ -65,5 +69,14 @@ public class OrganiserSendNotificationUIFragment extends Fragment {
                 notificationHeader.setText("Cancelled Entrants Notification");
                 break;
         }
+
+        // Send the notification
+        String messageContent = notificationMessage.getText().toString();
+
+        if (messageContent.isEmpty()) {
+            notificationMessage.setError("Message is required");
+        }
+
+        // Add notification to database here
     }
 }

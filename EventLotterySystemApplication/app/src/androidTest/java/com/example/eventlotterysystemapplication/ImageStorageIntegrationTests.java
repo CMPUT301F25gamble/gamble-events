02e50aecde98779b14705d9e07fbe9360ff17c96
Daniel <167367785+u-iDaniel@users.noreply.github.com>
2025-11-11 21:56:24 -0700

    Finish image storage class + integration tests

---
+++ EventLotterySystemApplication/app/src/androidTest/java/com/example/eventlotterysystemapplication/ImageStorageIntegrationTests.java
@@ -0,0 +1,89 @@
+package com.example.eventlotterysystemapplication;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+import android.net.Uri;
+import android.util.Log;
+
+import com.google.android.gms.tasks.Tasks;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.ExecutionException;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class ImageStorageIntegrationTests {
+
+    private ImageStorage imgStore;
+
+    @Before
+    public void setup() {
+        imgStore = ImageStorage.getInstance();
+    }
+
+    @Test
+    public void testUploadImg() throws FileNotFoundException, ExecutionException, InterruptedException {
+        String eventId = "eventID123wowsooriginal";
+
+        // Create a temp image file from an online url
+        File testImage = null;
+        try {
+            URL url = new URL("https://i.imgur.com/b6pVsrn.jpeg");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.connect();
+
+            InputStream input = conn.getInputStream();
+            testImage = File.createTempFile("test_image", ".jpg");
+            OutputStream output = new FileOutputStream(testImage);
+
+            byte[] buffer = new byte[4096];
+            int bytesRead;
+            while ((bytesRead = input.read(buffer)) != -1) {
+                output.write(buffer, 0, bytesRead);
+            }
+
+            output.close();
+            input.close();
+            conn.disconnect();
+        } catch (MalformedURLException e) {
+            Log.e("ImgStoreTests", "Malformed URL");
+        } catch (IOException e) {
+            Log.e("ImgStoreTests", "IOException opening connection");
+        }
+
+        if (testImage == null) {
+            throw new FileNotFoundException("Test Image File was not be created");
+        }
+
+        Uri imgUri = Tasks.await(imgStore.uploadEventPoster(eventId, testImage, task -> {
+            Uri uri = task.getResult();
+            Log.d("ImgStoreTestsInner", uri.toString());
+            assertTrue(uri.toString().contains(eventId));
+        }));
+
+        Log.d("ImgStoreTests", imgUri.toString());
+        assertTrue(imgUri.toString().contains(eventId));
+    }
+
+    @Test
+    public void testUploadNonImg() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            imgStore.uploadEventPoster("123", new File("test.txt"), task -> {});
+        });
+    }
+}

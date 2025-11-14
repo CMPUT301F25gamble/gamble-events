f84115501a5ce6c30e2cdc8f76101e7aa4824c6a
Ostrich <theamazingostrich1231@gmail.com>
2025-11-14 09:30:03 -0700

    Added code to go directly to correct event detail screen from notification

--- EventLotterySystemApplication/app/src/main/java/com/example/eventlotterysystemapplication/Model/LotteryFirebaseMessagingService.java
+++ EventLotterySystemApplication/app/src/main/java/com/example/eventlotterysystemapplication/Model/LotteryFirebaseMessagingService.java
@@ -14,6 +14,7 @@ import android.widget.RemoteViews;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
+import com.example.eventlotterysystemapplication.Controller.ContentActivity;
 import com.example.eventlotterysystemapplication.R;
 import com.example.eventlotterysystemapplication.View.EventDetailScreenFragment;
 import com.google.firebase.messaging.FirebaseMessagingService;
@@ -52,7 +53,7 @@ public class LotteryFirebaseMessagingService extends FirebaseMessagingService {
 
         // TODO Do the intent that is triggered when the notification is tapped
         // Intent that triggers when the notification is tapped
-        Intent intent = new Intent(this, EventDetailScreenFragment.class);
+        Intent intent = new Intent(this, ContentActivity.class);
 
         // add eventId to the intent
         intent.putExtra("eventId", remoteMessage.getData().get("eventID"));

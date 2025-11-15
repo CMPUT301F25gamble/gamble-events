0e973fe1d804fcf8b609719fe3bf65b38e087695
Cameron L. <imcelarson@gmail.com>
2025-11-14 18:16:24 -0700

    Working pop up

--- EventLotterySystemApplication/app/src/main/java/com/example/eventlotterysystemapplication/View/PendingEntrantListFragment.java
+++ EventLotterySystemApplication/app/src/main/java/com/example/eventlotterysystemapplication/View/PendingEntrantListFragment.java
@@ -1,11 +1,15 @@
 package com.example.eventlotterysystemapplication.View;
 
+import android.app.AlertDialog;
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -13,6 +17,7 @@ import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.eventlotterysystemapplication.Model.Database;
+import com.example.eventlotterysystemapplication.Model.EntrantList;
 import com.example.eventlotterysystemapplication.Model.Event;
 import com.example.eventlotterysystemapplication.Model.User;
 import com.example.eventlotterysystemapplication.R;
@@ -35,6 +40,9 @@ public class PendingEntrantListFragment extends Fragment {
     private ArrayList<CharSequence> data = new ArrayList<>();
     // Adapter for listview
     private ArrayAdapter<CharSequence> adapter;
+    // List of pending entrants
+    private final ArrayList<User> pendingEntrants = new ArrayList<>();
+
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
@@ -113,10 +121,70 @@ public class PendingEntrantListFragment extends Fragment {
     private void loadPendingEntrantsIntoList(Event event) {
         // Loop through all pending entrants
         for (User u : event.getEntrantList().getCancelled()) {
+            pendingEntrants.add(u);     // Add user details to the list
             String name = u.getName();
-            data.add(name);
+            data.add(name);             // Add ONLY user's name to the list
         }
         // Notify the adapter
         adapter.notifyDataSetChanged();
+
+        // When you tap a name in the list, show the user details popup:
+        binding.pendingListOfEntrantsListView.setOnItemClickListener((parent, view, position, id) -> {
+            User selectedUser = pendingEntrants.get(position);
+            showPendingEntrantDialog(selectedUser);
+        });
     }
+
+    private void showPendingEntrantDialog(User user) {
+        LayoutInflater inflater = LayoutInflater.from(requireContext());
+        View dialogView = inflater.inflate(R.layout.dialog_pending_entrant_list, null);
+
+        TextView nameText = dialogView.findViewById(R.id.enterName);
+        TextView emailText = dialogView.findViewById(R.id.enterEmail);
+        TextView phoneText = dialogView.findViewById(R.id.enterPhone);
+        // TODO: implement Geolocation
+        // TextView locationText = dialogView.findViewById(R.id.enterLocation);
+        Button declineButton = dialogView.findViewById(R.id.dialogDeclineButton);
+        Button backButton = dialogView.findViewById(R.id.dialogBackButton);
+
+        nameText.setText(user.getName());
+        emailText.setText(user.getEmail());
+
+        // If phone number null, don't display
+        if (user.getPhoneNumber() == null) {
+            phoneText.setVisibility(View.GONE);
+            dialogView.findViewById(R.id.dialogUserPhone).setVisibility(View.GONE);
+        } else {
+            phoneText.setText(user.getPhoneNumber());
+        }
+
+        // TODO: implement Geolocation
+        // locationText.setText(user.getLocation());
+
+        AlertDialog dialog = new AlertDialog.Builder(requireContext())
+                .setView(dialogView)
+                .setCancelable(true)
+                .create();
+
+        // Set the background to transparent so we can show the rounded corners
+        dialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+
+        // Return to list view
+        backButton.setOnClickListener(v -> dialog.dismiss());
+
+        // TODO: make new dialog screen
+        // Move to a new dialog
+        declineButton.setOnClickListener(v -> {
+            // TODO: call method to reject an invite
+
+//            Toast.makeText(requireContext(),
+//                    "Invite declined for " + user.getName(),
+//                    Toast.LENGTH_SHORT).show();
+//
+//            dialog.dismiss();
+        });
+        dialog.show();
+    }
+
+
 }

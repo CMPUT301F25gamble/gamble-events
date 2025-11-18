a41dbeaeff3b7dfd843d59edd8eea15c8f76ea9a
Heiby <whl@ualberta.ca>
2025-11-18 14:28:52 -0700

    created AdminUnitTest and NotificationUnitTest

---
+++ EventLotterySystemApplication/app/src/test/java/com/example/eventlotterysystemapplication/AdminUnitTest.java
@@ -0,0 +1,20 @@
+package com.example.eventlotterysystemapplication;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class AdminUnitTest {
+
+    @Before
+    public void setUp() {
+
+    }
+
+    @After
+    public void tearDown() {
+
+    }
+}
---
+++ EventLotterySystemApplication/app/src/test/java/com/example/eventlotterysystemapplication/NotificationUnitTest.java
@@ -0,0 +1,21 @@
+package com.example.eventlotterysystemapplication;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class NotificationUnitTest {
+
+    @Before
+    public void setUp() {
+
+    }
+
+    @After
+    public void tearDown() {
+
+    }
+}
+

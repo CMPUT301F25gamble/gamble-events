cc5804b2bd6e5d86cd15e16e3ec9816cfe079fb1
Heiby <whl@ualberta.ca>
2025-11-18 15:10:10 -0700

    added ignore flag

--- EventLotterySystemApplication/app/src/test/java/com/example/eventlotterysystemapplication/NotificationUnitTest.java
+++ EventLotterySystemApplication/app/src/test/java/com/example/eventlotterysystemapplication/NotificationUnitTest.java
@@ -2,10 +2,12 @@ package com.example.eventlotterysystemapplication;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
+@Ignore // No testcases yet, remove this flag when new testcases are added
 public class NotificationUnitTest {
 
     @Before

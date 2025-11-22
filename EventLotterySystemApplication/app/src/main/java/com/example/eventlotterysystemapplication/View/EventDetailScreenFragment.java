cc290426b862c2dc844ce771a7ea6fb7de69be93
Cameron L. <imcelarson@gmail.com>
2025-11-22 16:33:56 -0700

    added strings from function I will add later, and removed owenership toast

--- EventLotterySystemApplication/app/src/main/java/com/example/eventlotterysystemapplication/View/EventDetailScreenFragment.java
+++ EventLotterySystemApplication/app/src/main/java/com/example/eventlotterysystemapplication/View/EventDetailScreenFragment.java
@@ -364,7 +364,6 @@ public class EventDetailScreenFragment extends Fragment {
      * @param userInWaitlist Boolean whether user is in waitlist of event or not
      */
     private void changeWaitlistBtn(boolean userInWaitlist) {
-        Toast.makeText(getContext(), "Ownership: " + isOwnedEvent, Toast.LENGTH_SHORT).show();
         if (isOwnedEvent) {
             binding.navigationBarButton.setText("Edit Event");
             binding.navigationBarButton.setBackgroundTintList(

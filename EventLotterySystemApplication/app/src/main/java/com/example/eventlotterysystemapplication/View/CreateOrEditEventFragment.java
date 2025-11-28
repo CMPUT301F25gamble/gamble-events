452360be96e270a414991ff1b7825f643daa7454
Heiby <whl@ualberta.ca>
2025-11-27 17:59:08 -0700

    fixed navigation bug

--- EventLotterySystemApplication/app/src/main/java/com/example/eventlotterysystemapplication/View/CreateOrEditEventFragment.java
+++ EventLotterySystemApplication/app/src/main/java/com/example/eventlotterysystemapplication/View/CreateOrEditEventFragment.java
@@ -28,6 +28,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavHost;
 import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.eventlotterysystemapplication.Controller.LotteryDrawScheduler;
@@ -418,7 +419,9 @@ public class CreateOrEditEventFragment extends Fragment {
                                                 // Return to events page if no poster was uploaded
                                                 Log.d(TAG, "No poster after adding event, going straight to event page...");
                                                 Bundle args = new Bundle();
-                                                navigateFromCreateEditEvent(event);
+//                                                navigateFromCreateEditEvent(event);
+                                                NavHostFragment.findNavController(CreateOrEditEventFragment.this)
+                                                        .navigateUp();
                                             }
                                             LotteryDrawScheduler lotteryDrawScheduler = new LotteryDrawScheduler();
                                             lotteryDrawScheduler.scheduleUpdateLotteryDraw(v.getContext(),event);
@@ -439,7 +442,9 @@ public class CreateOrEditEventFragment extends Fragment {
                                         } else {
                                             // Return to events page if no poster was uploaded
                                             Log.d(TAG, "No poster after adding event, going straight to event page...");
-                                            navigateFromCreateEditEvent(event);
+//                                            navigateFromCreateEditEvent(event);
+                                            NavHostFragment.findNavController(CreateOrEditEventFragment.this)
+                                                    .navigate(R.id.action_create_or_edit_event_fragment_to_events_ui_fragment);
                                         }
                                         LotteryDrawScheduler lotteryDrawScheduler = new LotteryDrawScheduler();
                                         lotteryDrawScheduler.scheduleNewLotteryDraw(v.getContext(),event);
@@ -619,31 +624,35 @@ public class CreateOrEditEventFragment extends Fragment {
                         database.updateEvent(event, task -> {
                             if (task.isSuccessful()) {
                                 // Return to events page
-                                navigateFromCreateEditEvent(event);
+//                                navigateFromCreateEditEvent(event);
+                                NavHostFragment.findNavController(CreateOrEditEventFragment.this)
+                                        .navigate(R.id.action_create_or_edit_event_fragment_to_events_ui_fragment);
                             }
                         });
 
                     } else {
                         Toast.makeText(getContext(), "Poster upload to Storage Bucket failed.", Toast.LENGTH_SHORT).show();
                         // Return to events page anyways as event is created
-                        navigateFromCreateEditEvent(event);
+//                        navigateFromCreateEditEvent(event);
+                        NavHostFragment.findNavController(CreateOrEditEventFragment.this)
+                                .navigate(R.id.action_create_or_edit_event_fragment_to_events_ui_fragment);
                     }
                 }
         );
     }
 
-    private void navigateFromCreateEditEvent(Event event){
-        String argEventId=eventId!=null? eventId:event!=null?event.getEventID():null;
-        if(argEventId!=null) {
-                        Bundle args = new Bundle();
-            args.putString("eventId", argEventId);
-                        NavHostFragment.findNavController(CreateOrEditEventFragment.this)
-                                .navigate(R.id.action_create_or_edit_event_fragment_to_my_event_detail_screen,args);
-        }else{
-            NavHostFragment.findNavController(CreateOrEditEventFragment.this)
-                    .navigate(R.id.action_create_or_edit_event_fragment_to_my_events_fragment);
-                    }
-    }
+//    private void navigateFromCreateEditEvent(Event event){
+//        String argEventId=eventId!=null? eventId:event!=null?event.getEventID():null;
+//        if(argEventId!=null) {
+//                        Bundle args = new Bundle();
+//            args.putString("eventId", argEventId);
+//                        NavHostFragment.findNavController(CreateOrEditEventFragment.this)
+//                                .navigate(R.id.action_create_or_edit_event_fragment_to_my_event_detail_screen,args);
+//        }else{
+//            NavHostFragment.findNavController(CreateOrEditEventFragment.this)
+//                    .navigate(R.id.action_create_or_edit_event_fragment_to_my_events_fragment);
+//                    }
+//    }
 
     /**
      * Creates a file from the given uri
